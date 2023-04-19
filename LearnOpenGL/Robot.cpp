ed861ba02ff6c38c1b183d16ceab55704a9642ea
Suchan Lee <lsc1814@naver.com>
2023-04-19 16:05:22 +0900

    commit : Study Robot Animation

--- LearnOpenGL/Robot.cpp
+++ LearnOpenGL/Robot.cpp
@@ -2,6 +2,17 @@
 #include <glut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
+#include <math.h>
+
+double time = 0;
+
+GLfloat globalRot = 0.0f;
+
+GLfloat headAngle = 0.0f;
+GLfloat armAngleL = 0.0f;
+GLfloat armAngleR = 0.0f;
+GLfloat legAngleL = 0.0f;
+GLfloat legAngleR = 0.0f;
 
 void drawLeftArm();
 void drawRightArm();
@@ -11,6 +22,26 @@ void drawHead();
 void drawBody();
 void MyDisplay();
 
+void MyTimer(int Value)
+{
+	time = time + 0.1;
+
+	globalRot += 1.0;
+	headAngle = 20 * sin(time*0.8f);
+	armAngleL = 20 * sin(time);
+	armAngleR = -20 * sin(time);
+	legAngleL = -20 * sin(time);
+	legAngleR = 20 * sin(time);
+
+	if (headAngle >= 360) headAngle -= 360;
+	if (armAngleL >= 360) armAngleL -= 360;
+	if (armAngleR >= 360) armAngleR -= 360;
+	if (legAngleL >= 360) legAngleL -= 360;
+	if (legAngleR >= 360) legAngleR -= 360;
+
+	glutPostRedisplay();
+	glutTimerFunc(20, MyTimer, 1);
+}
 void MyKeyboard(unsigned char KeyPressed, int X, int Y)
 {
 	switch (KeyPressed) {
@@ -37,6 +68,7 @@ int main(int argc, char** argv)
 
 	glutDisplayFunc(MyDisplay);
 	glutKeyboardFunc(MyKeyboard);
+	glutTimerFunc(20, MyTimer, 1);
 	glutMainLoop();
 	return 0;
 }
@@ -47,7 +79,8 @@ void MyDisplay()
 	glLoadIdentity();
 	// Test Enable or Disable
 	// 카메라 셋팅(시점조절관련 간단히 타이핑해볼것).
-	gluLookAt(3, 3, 3, 0, 0, 0, 0, 1, 0);
+	gluLookAt(6, 6, 6, 0, 0, 0, 0, 1, 0); // 우상단
+	//gluLookAt(-3, 0, 0, 0, 0, 0, 0, 1, 0); // 우측면
 	
 	// Axis
 	glBegin(GL_LINES);
@@ -55,6 +88,19 @@ void MyDisplay()
 	glColor3f(0.0f, 1.0f, 0.0f); glVertex3f(0.0f, 0.0f, 0.0f); glVertex3f(0.0f, 5.0f, 0.0f);
 	glColor3f(0.0f, 0.0f, 1.0f); glVertex3f(0.0f, 0.0f, 0.0f); glVertex3f(0.0f, 0.0f, 5.0f);
 	glEnd();
+
+	// Floor
+	glBegin(GL_QUADS);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(8.0f, -5.0f, -8.0f);
+	glVertex3f(8.0f, -5.0f, 8.0f);
+	glVertex3f(-8.0f, -5.0f, 8.0f);
+	glVertex3f(-8.0f, -5.0f, -8.0f);
+	glEnd();
+
+	glRotatef(globalRot, 0.0f, 1.0f, 0.0f);
+	glTranslatef(-6.0f, 0.0f, 0.0f);
+	
 	// 몸통
 	drawBody();
 	// 머리
@@ -65,12 +111,16 @@ void MyDisplay()
 	// 다리
 	drawLeftLeg();
 	drawRightLeg();
+
 	glFlush();
 }
 void drawHead()
 {
 	glPushMatrix();
 		glColor3f(1.0, 0.0, 0.0);
+		// Transform
+		glRotatef(headAngle, 0.0f, 1.0f, 0.0f);
+
 		glTranslatef(0.0, 2.8, 0.0);
 		glRotatef(-90, 0.0, 1.0, 0.0);
 ;		glutSolidTeapot(0.8);
@@ -85,63 +135,100 @@ void drawBody()
 		glutSolidCube(1.0);
 	glPopMatrix();
 }
-void drawLeftArm()
-{
-	glPushMatrix();
-		glColor3f(1.0, 1.0, 0.0);
-		glTranslatef(-1.5, 1.0, 0.0);
-		glScalef(0.5, 2.0, 0.5);
-		glutSolidCube(1.0);
-	glPopMatrix();
-	glPushMatrix();
-		glColor3f(0.0, 0.0, 0.0);
-		glTranslatef(-1.5, -0.3, 0.0);
-		glScalef(0.3, 0.3, 0.3);
-		glutSolidSphere(1.0, 12, 12);
-	glPopMatrix();
-}
 void drawRightArm()
 {
 	glPushMatrix();
-		glColor3f(0.0, 1.0, 0.0);
-		glTranslatef(1.5, 1.0, 0.0);
-		glScalef(0.5, 2.0, 0.5);
-		glutSolidCube(1.0);
-	glPopMatrix();
-	glPushMatrix();
-		glColor3f(0.0, 0.0, 0.0);
-		glTranslatef(1.5, -0.3, 0.0);
-		glScalef(0.3, 0.3, 0.3);
-		glutSolidSphere(1.0, 12, 12);
+		// Transform
+		glTranslatef(-1.5f, 2.0f, 0.0f);
+		glRotatef(armAngleR, 1.0f, 0.0f, 0.0f);
+
+		// Modeling
+		glPushMatrix();
+			glColor3f(1.0, 1.0, 0.0);
+			glTranslatef(0.0f, -1.0f, 0.0f);
+			glScalef(0.5, 2.0, 0.5);
+			glutSolidCube(1.0);
+		glPopMatrix();
+
+		// Modeling
+		glPushMatrix();
+			glColor3f(0.0, 0.0, 0.0);
+			glTranslatef(0.0f, -2.3f, 0.0f);
+			glScalef(0.3, 0.3, 0.3);
+			glutSolidSphere(0.8, 18, 18);
+		glPopMatrix();
 	glPopMatrix();
 }
-void drawLeftLeg()
+void drawLeftArm()
 {
 	glPushMatrix();
-		glColor3f(0.0, 1.0, 1.0);
-		glTranslatef(-0.5, -2.0, 0.0);
-		glScalef(0.6, 4.0, 0.8);
-		glutSolidCube(1.0);
-	glPopMatrix();
-	glPushMatrix();
-		glColor3f(0.0, 0.0, 0.0);
-		glTranslatef(-0.5, -4.3, 0.3);
-		glScalef(0.6, 0.5, 1.3);
-		glutSolidCube(1.0);
+		// Left Arm Transform
+		glTranslatef(1.5f, 2.0f, 0.0f);
+		glRotatef(armAngleL, 1.0f, 0.0f, 0.0f);
+		
+		// Left Arm Modeling
+		glPushMatrix();
+			glColor3f(0.0, 1.0, 0.0);
+			glTranslatef(0.0f, -1.0f, 0.0f);
+			glScalef(0.5, 2.0, 0.5);
+			glutSolidCube(1.0);
+		glPopMatrix();
+
+		// Left Hand Modeling
+		glPushMatrix();
+			glColor3f(0.0, 0.0, 0.0);
+			glTranslatef(0.0f, -2.3, 0.0);
+			glScalef(0.3, 0.3, 0.3);
+			glutSolidSphere(0.8, 18, 18);
+		glPopMatrix();
 	glPopMatrix();
 }
 void drawRightLeg()
 {
 	glPushMatrix();
-		glColor3f(0.0, 1.0, 1.0);
-		glTranslatef(0.5, -2.0, 0.0);
-		glScalef(0.6, 4.0, 0.8);
-		glutSolidCube(1.0);
+		// Transform
+		glTranslatef(-0.5f, 0.0f, 0.0f);
+		glRotatef(legAngleR, 1.0f, 0.0f, 0.0f);
+		
+		// Modeling
+		glPushMatrix();
+			glColor3f(0.0, 1.0, 1.0);
+			glTranslatef(0.0f, -2.0f, 0.0);
+			glScalef(0.6, 4.0, 0.8);
+			glutSolidCube(1.0);
+		glPopMatrix();
+
+		//Modeling
+		glPushMatrix();
+			glColor3f(0.0, 0.0, 0.0);
+			glTranslatef(0.0, -4.3, 0.3);
+			glScalef(0.6, 0.5, 1.3);
+			glutSolidCube(1.0);
+		glPopMatrix();
+
 	glPopMatrix();
+}
+void drawLeftLeg()
+{
 	glPushMatrix();
-		glColor3f(0.0, 0.0, 0.0);
-		glTranslatef(0.5, -4.3, 0.3);
-		glScalef(0.6, 0.5, 1.3);
-		glutSolidCube(1.0);
+		// Transform
+		glTranslatef(0.5f, 0.0f, 0.0f);
+		glRotatef(legAngleL, 1.0f, 0.0f, 0.0f);
+
+		// Modeling
+		glPushMatrix();
+			glColor3f(0.0, 1.0, 1.0);
+			glTranslatef(0.0, -2.0, 0.0);
+			glScalef(0.6, 4.0, 0.8);
+			glutSolidCube(1.0);
+		glPopMatrix();
+
+		// Modeling
+		glPushMatrix();
+			glColor3f(0.0, 0.0, 0.0);
+			glTranslatef(0.0f, -4.3, 0.3);
+			glScalef(0.6, 0.5, 1.3);
+			glutSolidCube(1.0);
+		glPopMatrix();
 	glPopMatrix();
 }
